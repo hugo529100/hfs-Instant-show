a5c827c35f9d45d9bbba852fa8d836d9ca8a44d9
Hug3O <131569471+hugo529100@users.noreply.github.com>
2025-05-15 14:09:18 +0800

    Add files via upload

---
+++ dist/plugin.js
@@ -0,0 +1,5 @@
+exports.version = 1;
+exports.apiRequired = 8.87;
+exports.description = "Auto-show media by clicking icons, and click the file name to open the file menu.";
+exports.repo = "Hug3O/Instant-show"
+exports.frontend_js = 'main.js';
